cf4a4dd1956e0a35a9167502c82acda83e545ad2
Jeff McCarrell <jeff@mccarrell.org>
2015-06-07 23:37:52 -0700

    add Jeffs dotfiles: ack, emacs, tmux

---
+++ .emacs-custom-23.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default-frame-alist (quote ((font . "Monospace-11") (menu-bar-lines . 1) (tool-bar-lines . 0) (vertical-scroll-bars) (width . 100) (height . 64) (foreground-color . "white") (background-color . "black") (cursor-color . "DarkOrange"))))
+ '(fill-column 94)
+ '(ns-alternate-modifier (quote meta))
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(p4-use-p4config-exclusively t)
+ '(paren-match-face (quote paren-face-match-light))
+ '(paren-sexp-mode t))
+
