a439bcf5508ebc1328d263b090c94c5f144320d0
Jeff McCarrell <jeff@mccarrell.org>
2015-06-27 15:51:30 -0700

    remove frame variables added by customize; single source of frame variables is my .emacs

--- .emacs.jwm.d/emacs-custom-24.el
+++ .emacs.jwm.d/emacs-custom-24.el
@@ -1,9 +1,11 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default-frame-alist (quote ((font . "Monospace-11") (menu-bar-lines . 1) (tool-bar-lines . 0) (vertical-scroll-bars) (width . 100) (height . 64) (foreground-color . "white") (background-color . "black") (cursor-color . "DarkOrange"))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("87166c71bfc0a1aaa9b4f54167c81ce2607211aa71d77c74aa932b04808c8f06" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(fill-column 94)
  '(ns-alternate-modifier (quote meta))
  '(ns-tool-bar-display-mode nil t)
@@ -12,3 +14,9 @@
  '(paren-match-face (quote paren-face-match-light))
  '(paren-sexp-mode t))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
