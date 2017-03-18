58f990dfdb61f72b45615d7616e0b41e9d5bebb4
Jeff McCarrell <jeff@mccarrell.org>
2017-03-17 18:16:44 -0700

    emacs: store settings as emacs writes it.

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -6,6 +6,9 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode xterm-color use-package solarized-theme pyvenv markdown-mode macrostep helm-swoop helm-ag avy ag ack)))
  '(user-full-name "Jeff McCarrell")
  '(user-initials "jwm")
  '(user-mail-address "jeff@mccarrell.org"))
@@ -14,5 +17,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "DeepSkyBlue1" :slant italic))))
- )
+ '(font-lock-comment-face ((t (:foreground "DeepSkyBlue1" :slant italic)))))
