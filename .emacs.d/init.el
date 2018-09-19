93faf5913a1f2b94f70a0609ee842308efa78015
Jeff McCarrell <jeff@mccarrell.org>
2018-09-19 08:24:13 -0700

    add expand-region to my emacs config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -533,6 +533,39 @@
           (add-to-list 'auto-mode-alist
                        '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))))
 
+;; try expand-region, with Howard Abrams customization of it
+;;  this is directly from Howards config
+(use-package expand-region
+  :ensure t
+  :config
+  (defun ha/expand-region (lines)
+    "Prefix-oriented wrapper around Magnar's `er/expand-region'.
+
+     Call with LINES equal to 1 (given no prefix), it expands the
+     region as normal.  When LINES given a positive number, selects
+     the current line and number of lines specified.  When LINES is a
+     negative number, selects the current line and the previous lines
+     specified.  Select the current line if the LINES prefix is zero."
+    (interactive "p")
+    (cond ((= lines 1)   (er/expand-region 1))
+          ((< lines 0)   (ha/expand-previous-line-as-region lines))
+          (t             (ha/expand-next-line-as-region (1+ lines)))))
+
+  (defun ha/expand-next-line-as-region (lines)
+    (message "lines = %d" lines)
+    (beginning-of-line)
+    (set-mark (point))
+    (end-of-line lines))
+
+  (defun ha/expand-previous-line-as-region (lines)
+    (end-of-line)
+    (set-mark (point))
+    (beginning-of-line (1+ lines)))
+
+  ;; jwm: however, I can't seem to get C-= from my mac keyboard.
+  ;;   so prefer C-c =
+  :bind ("C-c =" . ha/expand-region))
+
 ;; this is Howard Abrams flycheck config
 (use-package flycheck
   :ensure t
